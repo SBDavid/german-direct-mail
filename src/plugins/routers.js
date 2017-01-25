b1340e23d230603ea5595d324b253e491f3129c3
SBDavid <billl89@126.com>
2017-01-25 11:17:30 +0800

    Initial commit

---
+++ src/plugins/routers.js
@@ -0,0 +1,32 @@
+/* eslint-disable */
+import Vue from 'vue';
+import Router from 'vue-router';
+
+Vue.use(Router);
+
+export const router = new Router({
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      redirect: {
+        name: 'index',
+      },
+    },
+
+    {
+      path: 'index',
+      name: 'index',
+      component: resolve => require(['../components/Hello.vue'], resolve),
+    },
+    {
+      path: 'index1',
+      name: 'index1',
+      component: resolve => require(['../components/Hello.vue'], resolve),
+    },
+    {
+      path: '*',
+      component: resolve => require(['../components/common/404.vue'], resolve),
+    },
+  ],
+});
